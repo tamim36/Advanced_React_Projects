499dbb6fcdfd1398ff070a70c49f6cc2a1683750
tamim <tamimarefinanik@gmail.com>
2024-02-11 20:41:40 +0600

    adding states to set input field val to name

--- time-challenger-game/src/components/Player.jsx
+++ time-challenger-game/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+
 export default function Player() {
+  const [ playerName, setPlayerName ] = useState('')
+  const [ submitted, setSubmitted ] = useState(false)
+
+  const onClickSetPlayerName = () => {
+    setSubmitted(true)
+  }
+
+  const handleChange = (e) => {
+    setPlayerName(e.target.value)
+    setSubmitted(false)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : 'Unknown player'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={onClickSetPlayerName} >Set Name</button>
       </p>
     </section>
   );
